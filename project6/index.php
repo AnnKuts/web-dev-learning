a5c10ab5c39ad034f577f83a8233eef9d5f1e948
Ann Kuts <kuutsann@gmail.com>
2026-02-21 20:10:37 +0200

    chore: changed naming

--- project6/index.php
+++ project6/index.php
@@ -34,12 +34,6 @@ $postsToShow = array_slice($posts, -$limit);
 <body>
 <h1>Mini Blog</h1>
 
-<p>
-    Theme:
-    <a href="index.php?theme=light">light</a> |
-    <a href="index.php?theme=dark">dark</a>
-</p>
-
 <?php if (!$user): ?>
     <a href="login.php">Login</a>
 <?php else: ?>
@@ -47,12 +41,6 @@ $postsToShow = array_slice($posts, -$limit);
     <a href="create.php">Create Post</a> |
     <a href="logout.php">Logout</a>
 
-    <p>
-        Show:
-        <a href="index.php?limit=5">5</a> |
-        <a href="index.php?limit=10">10</a> |
-        <a href="index.php?limit=20">20</a>
-    </p>
 
     <h3>Posts:</h3>
     <?php foreach ($postsToShow as $post): ?>
@@ -60,6 +48,13 @@ $postsToShow = array_slice($posts, -$limit);
     <?php endforeach; ?>
 
     <?php $logger->log("User visited index"); ?>
+
+    <p>
+        Posts:
+        <a href="index.php?limit=5">5</a> |
+        <a href="index.php?limit=10">10</a> |
+        <a href="index.php?limit=20">20</a>
+    </p>
 <?php endif; ?>
 
 </body>
