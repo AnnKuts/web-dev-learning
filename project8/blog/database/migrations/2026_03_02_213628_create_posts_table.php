c0ded628bd9abe40549c0b2ef49f73a3b1a11a50
Ann Kuts <kuutsann@gmail.com>
2026-03-03 14:08:19 +0600

    feat: fill post table and model

--- project8/blog/database/migrations/2026_03_02_213628_create_posts_table.php
+++ project8/blog/database/migrations/2026_03_02_213628_create_posts_table.php
@@ -13,7 +13,12 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('header');
+            $table->text('description');
+            $table->foreignId('user_id')
+                ->constrained();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
